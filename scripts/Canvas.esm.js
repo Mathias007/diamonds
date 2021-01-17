ee8942ce898ef09507b4652555e01c05faa7fc3b
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-17 21:07:42 +0100

    Draw background and texts on canvas.

--- scripts/Canvas.esm.js
+++ scripts/Canvas.esm.js
@@ -1,4 +1,5 @@
 import { Common } from "./Common.esm.js";
+import { media } from "./Media.esm.js";
 
 export const CANVAS_WIDTH = 640;
 export const CANVAS_HEIGHT = 480;
@@ -17,6 +18,29 @@ class Canvas extends Common {
         this.context.canvas.height = CANVAS_HEIGHT;
         this.context.font = "20px Arial white";
         this.context.fillStyle = "white";
+    }
+
+    drawGameOnCanvas(gameState) {
+        this.drawBackground();
+        this.drawPointsToWin(gameState.pointsToWin);
+        this.drawPlayersPoints(gameState.getPlayerPoints());
+        this.drawLeftMovement(gameState.getLeftMovement());
+    }
+
+    drawBackground() {
+        this.context.drawImage(media.backgroundImage, 0, 0);
+    }
+
+    drawPointsToWin(pointsToWin) {
+        this.context.fillText(`${pointsToWin}`, 520, 92);
+    }
+
+    drawPlayersPoints(playerPoints) {
+        this.context.fillText(`${playerPoints}`, 520, 163);
+    }
+
+    drawLeftMovement(leftMovement) {
+        this.context.fillText(`${leftMovement}`, 520, 234);
     }
 }
 
